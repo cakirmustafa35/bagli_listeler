211993454e0dad973152b4d900cfb9c9b223a50f
Mustafa Çakır <enescakir31487@gmail.com>
2024-02-27 12:11:37 +0300

    LinkedList

---
+++ 1)CreateNode.cpp
@@ -0,0 +1,73 @@
+﻿//node sınıfı nasıl oluşur onu öğreniyoruz arkadaşlar
+
+#include <iostream>
+using namespace std;
+
+class Node {
+
+public:
+    int deger;
+    Node* next;
+
+    Node(int deger) {
+        this->deger = deger;
+        next = NULL;
+
+    }
+
+
+};
+
+class LinkedList {
+private:
+    Node* head;
+    Node* tail;
+    int count;
+
+public:
+    LinkedList(int deger) {
+        Node* node = new Node(deger);
+        head = node;
+        tail = node;
+        count = 1;//eleman sayısını belirtir 
+    }
+
+    ~LinkedList() {
+        cout << "Destructor is called " << endl;
+        Node* tmp = head;
+        while (head) {
+            head = head->next;
+            delete tmp;
+            tmp = head;
+        }
+
+    }
+
+
+    void printHead() {
+        if (head != NULL)//programı crahslamasın diye yazıldı
+            cout <<"Head :"<< head->deger << endl;
+
+    }
+    void printTail() {
+        if (head != NULL)
+            cout << "Tail :" << tail->deger << endl;
+    }
+    
+    void getCount() {
+        cout << "List Count : " << this->count << endl;
+    }
+
+};
+
+int main()
+{
+    LinkedList* linkedList1 = new LinkedList(12);
+    linkedList1->getCount();
+    linkedList1->printHead();
+    linkedList1->printTail();
+    delete linkedList1;
+
+
+}
+
---
+++ 2)AddNode.cpp
@@ -0,0 +1,96 @@
+﻿//listenin sonuna yeni node ekleme
+//tail yeni eleman göstericek şekil yazmak 
+//listenin başında herhangi bir eleman yoksa head ve tail yeni elemanı başa getirerek göstericek
+
+#include <iostream>
+using namespace std;
+
+class Node {
+
+public:
+    int deger;
+    Node* next;
+
+    Node(int deger) {
+        this->deger = deger;
+        next = NULL;
+
+    }
+
+
+};
+
+class LinkedList {
+private:
+    Node* head;
+    Node* tail;
+    int count;
+
+public:
+    LinkedList(int deger) {
+        Node* node = new Node(deger);
+        head = node;
+        tail = node;
+        count = 1;//eleman sayısını belirtir 
+    }
+
+    ~LinkedList() {
+        cout << "Destructor is called " << endl;
+        Node* tmp = head;
+        while (head) {
+            head = head->next;
+            delete tmp;
+            tmp = head;
+        }
+
+    }
+
+
+    void printHead() {
+        if (head != NULL)//programı crahslamasın diye yazıldı
+            cout << "Head :" << head->deger << endl;
+
+    }
+    void printTail() {
+        if (head != NULL)
+            cout << "Tail :" << tail->deger << endl;
+    }
+
+    void getCount() {
+        cout << "List Count : " << this->count << endl;
+    }
+
+    void appendList(int deger) {
+        Node* node = new Node(deger);// yeni nod oluşturuldu
+        
+
+        if (count == 0) {//eğer listemde hiç eleman yoksa yeni eleman head ve taili gösteicek
+             
+            head = node;
+            tail = node;
+            
+           
+        }
+        else { //eğer bir veya fazlası varsa yeni gelen eleman tail olucak
+
+            tail->next = node;
+            tail = node;
+        }
+        count++;//yeni değer eklenilceğinden count arttırılmalıdır 
+    }
+
+};
+
+int main()
+{
+    LinkedList* linkedList1 = new LinkedList(12);
+    linkedList1->appendList(7);
+
+    linkedList1->getCount();
+    linkedList1->printHead();
+    linkedList1->printTail();
+    delete linkedList1;
+
+
+}
+
